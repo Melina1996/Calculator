97fd98ab0a56f2f5899ed6e2824b95a219ada350
Melina1996 <melina.schlachter@gmx.de>
2024-01-22 14:39:32 +0100

    added loop to assign paragraph.innerTxt

--- public/js/main.js
+++ public/js/main.js
@@ -17,10 +17,6 @@ for (let i = 0; i <= 5; i++) {
   myOperators.appendChild(myOperatorButton)
 }
 
-let myResultButton = document.querySelectorAll(".my-button")[16]
-
-myResultButton.classList.add("my-result-operator")
-
 //add paragraph to each of the buttons
 
 let allButtons = document.querySelectorAll(".my-button")
@@ -31,27 +27,30 @@ allButtons.forEach(element => {
   element.appendChild(newP)
 });
 
-let allPs = document.querySelectorAll(".my-p-button")
 
-document.querySelectorAll(".my-p-button")[0].innerText = "7"
-document.querySelectorAll(".my-p-button")[1].innerText = "8"
-document.querySelectorAll(".my-p-button")[2].innerText = "9"
-document.querySelectorAll(".my-p-button")[3].innerText = "4"
-document.querySelectorAll(".my-p-button")[4].innerText = "5"
-document.querySelectorAll(".my-p-button")[5].innerText = "6"
-document.querySelectorAll(".my-p-button")[6].innerText = "1"
-document.querySelectorAll(".my-p-button")[7].innerText = "2"
-document.querySelectorAll(".my-p-button")[8].innerText = "3"
-document.querySelectorAll(".my-p-button")[9].innerText = "0"
-document.querySelectorAll(".my-p-button")[10].innerText = "."
-document.querySelectorAll(".my-p-button")[11].innerText = "C"
+let allNumbers = ["7","8","9","4","5","6","1","2","3","0",".","C"]
 
-document.querySelectorAll(".my-p-button")[12].innerText = "+"
-document.querySelectorAll(".my-p-button")[13].innerText = "-"
-document.querySelectorAll(".my-p-button")[14].innerText = "/"
-document.querySelectorAll(".my-p-button")[15].innerText = "*"
-document.querySelectorAll(".my-p-button")[16].innerText = "%"
-document.querySelectorAll(".my-p-button")[17].innerText = "="
+let pNumbers = document.querySelector(".my-numbers").querySelectorAll(".my-p-button")
+
+for (let i = 0; i <= 11; i++) {
+  pNumbers[i].innerHTML = allNumbers[i]
+}
+
+let allOperators = ["+","-","/","*","%","="]
+
+let pOperators = document.querySelector(".my-operators").querySelectorAll(".my-p-button")
+console.log(pOperators)
+
+for (let i = 0; i <= 5; i++) {
+    pOperators[i].innerHTML = allOperators[i]
+}
+
+//want my "="-button to be bigger than the other ones, add class
+
+let myResultButton = document.querySelectorAll(".my-button")[16]
+
+myResultButton.classList.add("my-result-operator")
+
 
 //declaration of variabled for all buttons
 
@@ -96,6 +95,7 @@ allButtons.forEach(element => {
         myArray.forEach(element => {
           number += element
         });
+
         try{
           result = eval(number)
           myPResult.innerText = result
